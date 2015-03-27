55858e37649af4cd2b76e2c451792be29cde64aa
iruslani <iruslani75@gmail.com>
2015-03-27 16:44:21 -0700

    Add post request.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var jobs = require('./routes/jobs');
-
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/jobs');
@@ -34,14 +34,27 @@ app.get('/', function(req, res) {
 app.get('/api/jobs', function(req, res){
   Jobs.find(function(err, docs) {
   	docs.forEach(function(item){
-  		console.log('Received a get request for _id ' + item._id + ' job: ' + item.job);
+  		console.log('Received a GET request for _id ' + item._id + ' job: ' + item.job);
   	});
   	res.send(docs);
   })
 })
 
+app.post('api/jobs', function(req, res){
+	console.log('Receive a POST request');
+	for (var key in req.body) {
+		console.log(key + ': ' + req.body[key]);		
+	};
+	var jobs = new Jobs(req.body);
+	jobs.save(function(err, doc){
+		res.send(doc);
+	});
+})
 
+// Use routes
 app.use('/jobs', jobs);
+// Setup body parser
+app.use(bodyParser.json());
 
 
 var port = 3000;
