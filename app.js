3b2ffca5bac4498c22497852e5ab22625367fc80
iruslani <iruslani75@gmail.com>
2015-03-28 01:08:38 -0700

    Got post working finally.

--- app.js
+++ app.js
@@ -2,34 +2,43 @@ var express = require('express');
 var jobs = require('./routes/jobs');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/jobs');
-var Schema = mongoose.Schema;
-
-var JobSchema = new Schema({
-	job: String,
-	Description: String,
-	Company: String
-});
-
-mongoose.model('Jobs', JobSchema);
-
-var Jobs = mongoose.model('Jobs');
-
-// Add initial data to the 
-/*var jobsInit = new Jobs({
-	job: 'Full-Stack Web Developer',
-	Description: 'Need a developer to create app using restfull API',
-	Company: 'MassDrop'
-});
-jobsInit.save();*/
+var port = process.env.PORT || 3000;
+var router = express.Router();
 
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
+// Setup body parser:
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+// Connect to DB:
+mongoose.connect('mongodb://localhost/jobs2');
+
+// Build schema:
+var Schema = mongoose.Schema;
+var JobSchema = new Schema({
+	title: String,
+	description: String,
+	company: String
+});
+
+var Jobs = mongoose.model('Jobs', JobSchema);
+
+// Add initial data:
+/*var jobsInit = new Jobs({
+	job: 'Full-Stack Web Developer',
+	description: 'Need a developer to create app using restfull API',
+	company: 'MassDrop'
+});
+jobsInit.save();*/
+
+
+
+// Respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world')
-})
+  res.send('hello world');
+});
 
 app.get('/api/jobs', function(req, res){
   Jobs.find(function(err, docs) {
@@ -38,25 +47,30 @@ app.get('/api/jobs', function(req, res){
   	});
   	res.send(docs);
   })
-})
+});
 
-app.post('api/jobs', function(req, res){
+app.post('/api/jobs', function(req, res){
 	console.log('Receive a POST request');
 	for (var key in req.body) {
 		console.log(key + ': ' + req.body[key]);		
 	};
-	var jobs = new Jobs(req.body);
+	var jobs = new Jobs();
+	jobs.title = req.body.title;
+	jobs.description = req.body.description;
+	jobs.company = req.body.company;
+	console.log('jobs:');
+	console.log(jobs);
+
 	jobs.save(function(err, doc){
-		res.send(doc);
+		if (err) res.send(err);
+		res.send({message: "Job created"});
 	});
 })
 
-// Use routes
+
+// Use routes:
 app.use('/jobs', jobs);
-// Setup body parser
-app.use(bodyParser.json());
 
 
-var port = 3000;
 app.listen(port);
 console.log('App serving on port' + port);
